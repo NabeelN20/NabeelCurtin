1199452491cf20adee1c0813c729c8f3ee52e2e8
Nabeel Nauzeer <20255000@student.curtin.edu.au>
2020-04-05 21:26:58 +0400

    changed symbols in viewer.java

--- Viewer.java
+++ Viewer.java
@@ -9,23 +9,23 @@ public class Viewer
                 switch(grid[i][j])
                 {
                     case Maze.EMPTY:
-                        System.out.print("  ");
+                        System.out.print("?");
                         break;
                         
                     case Maze.WALL:
-                        System.out.print("##");
+                        System.out.print("xx");
                         break;
                         
                     case Maze.START:
-                        System.out.print("^^");
+                        System.out.print("<>");
                         break;
                         
                     case Maze.END:
-                        System.out.print("$$");
+                        System.out.print("\/");
                         break;
                         
                     case Maze.VISITED:
-                        System.out.print("..");
+                        System.out.print("^");
                         break;
                         
                     default:
